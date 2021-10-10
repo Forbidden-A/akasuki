a101cc0a04b103aa065be39a86502408f5590cbb
Forbidden-A <Forbidden.BA@ProtonMail.com>
2021-10-10 15:08:56 +0300

    Fix panicking on startup

--- src/listeners/mod.rs
+++ src/listeners/mod.rs
@@ -2,8 +2,17 @@ mod ready;
 
 use poise::{BoxFuture, Event, Framework};
 use ready::ready;
+use tracing::debug;
 
 use crate::{AkasukiError, AkasukiResult};
+
+pub async fn log_event<'a>(
+    _ctx: &serenity::prelude::Context,
+    e: &'a Event<'_>,
+) -> AkasukiResult<()> {
+    debug!("Got event: {:?}", e);
+    Ok(())
+}
 
 pub fn listener<'a, A>(
     ctx: &'a serenity::prelude::Context,
@@ -13,6 +22,6 @@ pub fn listener<'a, A>(
 ) -> BoxFuture<'a, AkasukiResult<()>> {
     match event {
         Event::Ready { data_about_bot } => Box::pin(ready(ctx, data_about_bot)),
-        _ => todo!(),
+        _ => Box::pin(log_event(ctx, event)),
     }
 }
